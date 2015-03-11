74e6f30a531f0a1abbe2307eff862a73a28a52d4
Gaurav Kumar Singh <gaurav@kalycito.com>
2015-03-11 21:48:04 +0530

    [FEATURE] Add firmware update tool for APC/PPC2100
    - Add main file for the firmware update.
    - Add special user control module which implements only the necessary
      routines for firmware update.
    - Add default update firmware files image.bin and image.txt.
    
    Signed-off-by: Gaurav Kumar Singh <gaurav@kalycito.com>

---
+++ apps/firmware_update/src/ctrlu.h
@@ -0,0 +1,82 @@
+/**
+********************************************************************************
+\file   app/ctrlu.h
+
+\brief  Definitions for special user control module for firmware update tool
+
+This file contains the definitions for the user control module used by firmware
+update tool. The control module only implements control functions necessary to
+control kernel stack for downloading and invoking and update.
+
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2015, Kalycito Infotech Private limited
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+#ifndef _INC_ctrlu_H_
+#define _INC_ctrlu_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <common/oplkinc.h>
+#include <oplk/obd.h>
+#include <common/ctrl.h>
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+tOplkError ctrlu_init(void);
+void       ctrlu_exit(void);
+tOplkError ctrlu_initStack(tOplkApiInitParam* pInitParam_p);
+tOplkError ctrlu_shutdownStack(void);
+BOOL       ctrlu_checkKernelStack(void);
+tOplkError ctrlu_getKernelInfo(tCtrlKernelInfo* pKernelInfo_p);
+UINT8*     ctrlu_getEthMacAddr(void);
+BOOL       ctrlu_stackIsInitialized(void);
+tOplkError ctrlu_writeFileToKernel(tCtrlFileType type_p, INT length_p, UINT8* pBuffer_p);
+tOplkError ctrlu_setNextImageFlag(tCtrlFileType fileType_p);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INC_ctrlu_H_ */
---
+++ apps/firmware_update/src/main.c
@@ -0,0 +1,354 @@
+/**
+********************************************************************************
+\file   main.c
+
+\brief  Main file of firmware update tool for APC/PPC2100
+
+This file contains the main file of the openPOWERLINK firmware update tool.
+
+\ingroup module_fwupdate_tool
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2015, Kalycito Infotech Private Ltd.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+
+#include <oplk/oplk.h>
+#include <oplk/debugstr.h>
+#include <ctrlu.h>
+#include <system/system.h>
+#include <getopt/getopt.h>
+#include <console/console.h>
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define CYCLE_LEN         UINT_MAX
+#define NODEID            0xF0                //=> MN
+#define IP_ADDR           0xc0a86401          // 192.168.100.1
+#define SUBNET_MASK       0xFFFFFF00          // 255.255.255.0
+#define DEFAULT_GATEWAY   0xC0A864FE          // 192.168.100.C_ADR_RT1_DEF_NODE_ID
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+const BYTE aMacAddr_g[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#ifndef READ_FILE
+const unsigned char     aTestBuffer[] =
+{
+    #include "image.txt"
+};
+#endif
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+typedef struct
+{
+    char        fwImage[256];  
+} tOptions;
+
+typedef struct
+{
+    char*   pFwImageBuff;
+    INT     length;
+} tFirmwareImage;
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+static int getOptions(int argc_p, char** argv_p, tOptions* pOpts_p);
+static tOplkError initPowerlink(UINT32 cycleLen_p, const BYTE* macAddr_p);
+static void shutdownPowerlink(void);
+static tOplkError updateFirmwareImage(UINT8* pFwBuffer_p, INT length_p);
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief  main function
+
+This is the main function of the openPOWERLINK console MN demo application.
+
+\param  argc                    Number of arguments
+\param  argv                    Pointer to argument strings
+
+\return Returns an exit code
+
+\ingroup module_demo_mn_console
+*/
+//------------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    tOplkError                  ret = kErrorOk;
+    tOptions                    opts;
+    UINT32                      version;
+    tFirmwareImage              firmwareImage;
+
+    getOptions(argc, argv, &opts);
+
+    if (system_init() != 0)
+    {
+        fprintf(stderr, "Error initializing system!");
+        return 0;
+    }
+
+    version = PLK_DEFINED_STACK_VERSION;
+    printf("----------------------------------------------------\n");
+    printf("Firmware Update application for B&R APC/PPC2100\n");
+    printf("for openPOWERLINK Stack: %x.%x.%x\n", PLK_STACK_VER(version), PLK_STACK_REF(version), PLK_STACK_REL(version));
+    printf("----------------------------------------------------\n");
+
+    if ((ret = initPowerlink(CYCLE_LEN, aMacAddr_g)) != kErrorOk)
+    {
+        printf("POWERLINK initialization failed error 0x%2X\n", ret);
+        goto Exit;
+    }
+
+#ifdef READ_FILE
+    readFirmwareFile(opts.fwImage, &firmwareImage);
+#else
+    firmwareImage.pFwImageBuff = (unsigned char*) aTestBuffer;
+    firmwareImage.length = (INT)sizeof(aTestBuffer);
+#endif
+    updateFirmwareImage(firmwareImage.pFwImageBuff, firmwareImage.length);
+
+Exit:
+    shutdownPowerlink();
+
+    system_exit();
+
+    return 0;
+}
+
+//============================================================================//
+//            P R I V A T E   F U N C T I O N S                               //
+//============================================================================//
+/// \name Private Functions
+/// \{
+
+//------------------------------------------------------------------------------
+/**
+\brief  Initialize the openPOWERLINK stack
+
+The function initializes the openPOWERLINK stack.
+
+\param  cycleLen_p              Length of POWERLINK cycle.
+\param  macAddr_p               MAC address to use for POWERLINK interface.
+
+\return The function returns a tOplkError error code.
+*/
+//------------------------------------------------------------------------------
+static tOplkError initPowerlink(UINT32 cycleLen_p, const BYTE* macAddr_p)
+{
+    tOplkError                  ret = kErrorOk;
+    static tOplkApiInitParam    initParam;
+    static char                 devName[128];
+
+    printf("Initializing openPOWERLINK stack...\n");
+
+    memset(&initParam, 0, sizeof(initParam));
+    initParam.sizeOfInitParam = sizeof(initParam);
+
+    // pass selected device name to Edrv
+    initParam.hwParam.pDevName = devName;
+    initParam.nodeId = NODEID;
+    initParam.ipAddress = (0xFFFFFF00 & IP_ADDR) | initParam.nodeId;
+
+    /* write 00:00:00:00:00:00 to MAC address, so that the driver uses the real hardware address */
+    memcpy(initParam.aMacAddress, macAddr_p, sizeof(initParam.aMacAddress));
+
+    initParam.fAsyncOnly              = FALSE;
+    initParam.featureFlags            = UINT_MAX;
+    initParam.cycleLen                = cycleLen_p;       // required for error detection
+    initParam.isochrTxMaxPayload      = 256;              // const
+    initParam.isochrRxMaxPayload      = 256;              // const
+    initParam.presMaxLatency          = 50000;            // const; only required for IdentRes
+    initParam.preqActPayloadLimit     = 36;               // required for initialisation (+28 bytes)
+    initParam.presActPayloadLimit     = 36;               // required for initialisation of Pres frame (+28 bytes)
+    initParam.asndMaxLatency          = 150000;           // const; only required for IdentRes
+    initParam.multiplCylceCnt         = 0;                // required for error detection
+    initParam.asyncMtu                = 1500;             // required to set up max frame size
+    initParam.prescaler               = 2;                // required for sync
+    initParam.lossOfFrameTolerance    = 500000;
+    initParam.asyncSlotTimeout        = 3000000;
+    initParam.waitSocPreq             = 1000;
+    initParam.deviceType              = UINT_MAX;         // NMT_DeviceType_U32
+    initParam.vendorId                = UINT_MAX;         // NMT_IdentityObject_REC.VendorId_U32
+    initParam.productCode             = UINT_MAX;         // NMT_IdentityObject_REC.ProductCode_U32
+    initParam.revisionNumber          = UINT_MAX;         // NMT_IdentityObject_REC.RevisionNo_U32
+    initParam.serialNumber            = UINT_MAX;         // NMT_IdentityObject_REC.SerialNo_U32
+
+    initParam.subnetMask              = SUBNET_MASK;
+    initParam.defaultGateway          = DEFAULT_GATEWAY;
+    sprintf((char*)initParam.sHostname, "%02x-%08x", initParam.nodeId, initParam.vendorId);
+    initParam.syncNodeId              = C_ADR_SYNC_ON_SOA;
+    initParam.fSyncOnPrcNode          = FALSE;
+
+    // set callback functions
+    initParam.pfnCbEvent = NULL;
+    initParam.pfnCbSync  = NULL;
+
+    // initialize POWERLINK stack
+    ret = ctrlu_init();
+    if (ret != kErrorOk)
+    {
+        fprintf(stderr, "ctrlu_init() failed with \"%s\" (0x%04x)\n", debugstr_getRetValStr(ret), ret);
+        return ret;
+    }
+
+    ret = ctrlu_initStack(&initParam);
+    if (ret != kErrorOk)
+    {
+        fprintf(stderr, "oplk_init() failed with \"%s\" (0x%04x)\n", debugstr_getRetValStr(ret), ret);
+        return ret;
+    }
+
+    return kErrorOk;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Shutdown the kernel stack
+
+The function shuts down the kernel stack.
+*/
+//------------------------------------------------------------------------------
+static void shutdownPowerlink(void)
+{
+    printf("Shutdown Stack\n");
+    ctrlu_shutdownStack();
+    ctrlu_exit();
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Get command line parameters
+
+The function parses the supplied command line parameters and stores the
+options at pOpts_p.
+
+\param  argc_p                  Argument count.
+\param  argc_p                  Pointer to arguments.
+\param  pOpts_p                 Pointer to store options
+
+\return The function returns the parsing status.
+\retval 0           Successfully parsed
+\retval -1          Parsing error
+*/
+//------------------------------------------------------------------------------
+static int getOptions(int argc_p, char** argv_p, tOptions* pOpts_p)
+{
+    int                         opt;
+
+    /* setup default parameters */
+    strncpy(pOpts_p->fwImage, "image.bin", 256);
+
+    /* get command line parameters */
+    while ((opt = getopt(argc_p, argv_p, "i:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'i':
+                strncpy(pOpts_p->fwImage, optarg, 256);
+                break;
+            default: /* '?' */
+                printf("Usage: %s [-i UPDATE IMAGE] \n", argv_p[0]);
+                return -1;
+        }
+    }
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Write new firmware image
+
+The function copies the new firmware image into the update image location in
+flash.
+
+\param  pFwBuffer_p        Pointer to the buffer containing update image.
+\param  length_p           Length of the update image.
+
+\return The function returns the tOplkError error code.
+
+*/
+//------------------------------------------------------------------------------
+static tOplkError updateFirmwareImage(UINT8* pFwBuffer_p, INT length_p)
+{
+    tOplkError      ret;
+    tCtrlFileType   fileType = kCtrlFileTypeFirmwareUpdate;
+
+    do
+    {
+        console_printlog("Transfer test buffer to kernel stack with size %d...\n",
+                         (INT)sizeof(aTestBuffer));
+        ret = ctrlu_writeFileToKernel(fileType, length_p, pFwBuffer_p);
+        console_printlog("ctrlu_writeFileToKernel() returned with 0x%X\n", ret);
+
+        console_printlog("Set Update image\n");
+        ret = ctrlu_setNextImageFlag(fileType);
+
+        if (ret != kErrorOk)
+        {
+            console_printlog("Error in updating image 0x%2X\n", ret);
+        }
+
+    } while (ret != kErrorOk);
+
+    return ret;
+}
+
+/// \}
